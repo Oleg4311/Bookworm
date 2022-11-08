4b4bb00fd9f19826e9f28fa8838fc2f008b9da02
Oleg <olegbelii4311@gmail.com>
2022-11-08 10:58:38 +0300

    init project

---
+++ db/models/book.js
@@ -0,0 +1,27 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Book.init({
+    picture: DataTypes.TEXT,
+    nameBook: DataTypes.TEXT,
+    author: DataTypes.TEXT,
+    comments: DataTypes.TEXT,
+    userId: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'Book',
+  });
+  return Book;
+};
